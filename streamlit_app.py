9c8491df3afeee6d3b97c34b4ecb6c742fe83732
javierbengolea <javierbengolea@yahoo.com.ar>
2024-03-22 00:58:01 +0000

    ---

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import math
 from pathlib import Path
 
+st.header('Streamlit Dataset')
+
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='GDP Dashboard',
